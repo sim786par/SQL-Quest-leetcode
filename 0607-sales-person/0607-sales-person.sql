94c3763dcac0d82f5c67461632be29b34d55cfc0
Simran Parween <simranparween2754@gmail.com>
2026-04-08 18:37:39 +0530

    Time: 1412 ms (38.52%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
-select s.name 
-from Orders o
-join Company c on o.com_id = c.com_id and c.name = "RED"
-Right join SalesPerson s
-on s.sales_id = o.sales_id
-where o.sales_id is null;
+-- select s.name 
+-- from Orders o
+-- join Company c on o.com_id = c.com_id and c.name = "RED"
+-- Right join SalesPerson s
+-- on s.sales_id = o.sales_id
+-- where o.sales_id is null;
 
 -- select s.name 
 -- from SalesPerson s
@@ -15,3 +15,12 @@ where o.sales_id is null;
 --     left join Company c on o.com_id = c.com_id and
 --     where c.name = "RED"
 -- );
+
+SELECT s.name
+FROM SalesPerson s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE o.sales_id = s.sales_id AND c.name = 'RED'
+);
